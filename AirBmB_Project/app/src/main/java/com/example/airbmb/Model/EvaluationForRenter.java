d351f6928466a977f9917f4997f34345e78b3bbb
DikeaSot <48369123+DikeaSot@users.noreply.github.com>
2019-05-16 21:48:02 +0300

    AirBmB Project

---
+++ AirBmB_Project/app/src/main/java/com/example/airbmb/Model/EvaluationForRenter.java
@@ -0,0 +1,49 @@
+package com.example.airbmb.Model;
+
+import java.util.ArrayList;
+
+public class EvaluationForRenter extends Evaluation {
+    private int repairsValue = 0;
+    private static ArrayList<EvaluationForRenter> evaluationsForRentersList = new ArrayList<EvaluationForRenter>();
+    private static double averageFeedbackForRenters = 0;
+
+    public EvaluationForRenter(int feedback) {
+    	this(feedback,"",0);
+    }
+
+    public EvaluationForRenter(int feedback, String comments, int repairsValue) {
+        super(feedback,comments);
+        this.repairsValue = repairsValue;
+        evaluationsForRentersList.add(this);
+        recalculateAverage();
+    }
+
+    public void setRepairsValue(int repairsValue) { this.repairsValue = repairsValue; }
+
+    public int getRepairsValue() { return repairsValue; }
+
+    public static double getAverageFeedback() { return averageFeedbackForRenters; }
+
+    public static int getNumOfEvaluations() { return evaluationsForRentersList.size(); }
+
+    public boolean damages() {return repairsValue!=0; }
+
+    private static void recalculateAverage() {
+        averageFeedbackForRenters = 0;
+        int num = getNumOfEvaluations();
+        if(num!=0) {
+	        for (EvaluationForRenter e : evaluationsForRentersList) averageFeedbackForRenters += e.getFeedback();
+	        averageFeedbackForRenters /= num;
+	    }
+    }  
+    
+    public void remove() {
+    	if(!evaluationsForRentersList.isEmpty()) evaluationsForRentersList.remove(this);
+    	recalculateAverage();
+    }
+    
+    public static void removeAll() {
+    	evaluationsForRentersList.clear();
+    	recalculateAverage();
+    }
+}
---
+++ AirBmB_Project/app/src/main/java/com/example/airbmb/Model/Owner.java
@@ -0,0 +1,30 @@
+package com.example.airbmb.Model;
+
+import java.util.*;
+
+public class Owner extends User{
+    private int numOfOwners;
+    private static ArrayList<Owner> listOwner = new  ArrayList<>();
+    private static ArrayList<House> listHouse = new  ArrayList<>();
+
+
+    public Owner(String firstName, String lastName, String email, String IBAN, String username, String password){
+        super(firstName,lastName,email,IBAN,username,password);
+        listOwner.add(this);
+    }
+
+    public static int getNumOfOwners() {
+        return listOwner.size();
+    }
+
+    public void remove(){
+        listOwner.remove(this);
+    }
+
+    public static void removeAll() {
+    	listOwner.clear();
+    }
+    
+    
+    
+}
