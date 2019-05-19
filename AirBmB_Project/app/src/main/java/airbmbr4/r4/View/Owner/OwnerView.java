89e7e933f33e21a0657a4497d50b32400a9218f4
DikeaSot <48369123+DikeaSot@users.noreply.github.com>
2019-05-19 13:31:58 +0300

    Older versions deleted

--- AirBmB_Project/app/src/main/java/airbmbr4/r4/View/Owner/OwnerView.java
+++
@@ -1,7 +0,0 @@
-package airbmbr4.r4.View.Owner;
-
-import airbmbr4.r4.View.User.UserView;
-
-public interface OwnerView extends UserView {
-	public void setPresenter(OwnerPresenter presenter);
-}
--- AirBmB_Project/app/src/main/java/airbmbr4/r4/View/Renter/RenterView.java
+++
@@ -1,7 +0,0 @@
-package airbmbr4.r4.View.Renter;
-
-import airbmbr4.r4.View.User.UserView;
-
-public interface RenterView extends UserView {
-	public void setPresenter(RenterPresenter presenter);
-}
