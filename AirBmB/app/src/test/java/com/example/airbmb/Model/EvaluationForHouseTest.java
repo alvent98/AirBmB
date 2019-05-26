76a086c0e5791291b9eea56e2c98dca2e7b57b75
DikeaSot <48369123+DikeaSot@users.noreply.github.com>
2019-05-26 14:22:20 +0300

    Evaluation Tests Edited

--- AirBmB/app/src/test/java/com/example/airbmb/Model/EvaluationForHouseTest.java
+++ AirBmB/app/src/test/java/com/example/airbmb/Model/EvaluationForHouseTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class EvaluationForHouseTest {
 
     @Before
@@ -13,22 +15,40 @@ public class EvaluationForHouseTest {
 
     @Test
     public void testRemoveWithTwoEval() {
-        EvaluationForHouse e1 = new EvaluationForHouse(5);
-        new EvaluationForHouse(1);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForHouse e1 = new EvaluationForHouse(l,5);
+        new EvaluationForHouse(l,1);
         e1.remove();
         Assert.assertEquals(1,EvaluationForHouse.getNumOfEvaluations());
     }
 
     @Test
     public void testRemoveWithOneEval() {
-        EvaluationForHouse e = new EvaluationForHouse(5);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForHouse e = new EvaluationForHouse(l,5);
         e.remove();
         Assert.assertEquals(0,EvaluationForHouse.getNumOfEvaluations());
     }
 
     @Test
     public void testRemoveWithNoEval() {
-        EvaluationForHouse e = new EvaluationForHouse(5);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForHouse e = new EvaluationForHouse(l,5);
         e.remove();
         e.remove();
         Assert.assertEquals(0,EvaluationForHouse.getNumOfEvaluations());
@@ -36,19 +56,37 @@ public class EvaluationForHouseTest {
 
     @Test
     public void testEvaluationForHouseInt() {
-        new EvaluationForHouse(5);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        new EvaluationForHouse(l,5);
         Assert.assertEquals(1,EvaluationForHouse.getNumOfEvaluations());
     }
 
     @Test
     public void testEvaluationForHouseIntString() {
-        new EvaluationForHouse(0,"The french knights were hostile");
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        new EvaluationForHouse(l,0,"The french knights were hostile");
         Assert.assertEquals(1,EvaluationForHouse.getNumOfEvaluations());
     }
 
     @Test
     public void testSetAndGetCommentsAndFeedBack() {
-        EvaluationForHouse e = new EvaluationForHouse(3,"The french knights threw some animals towards our soldiers!");
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForHouse e = new EvaluationForHouse(l,3,"The french knights threw some animals towards our soldiers!");
         e.setComments("We fled these arrogant knights.");
         e.setFeedback(1);
         Assert.assertEquals(1,e.getFeedback());
@@ -57,36 +95,82 @@ public class EvaluationForHouseTest {
 
     @Test
     public void testHashCodeWhenEquals() {
-        EvaluationForHouse e1 = new EvaluationForHouse(2,"They said they didn't elect King Arthur, so they don't obey!");
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForHouse e1 = new EvaluationForHouse(l,2,"They said they didn't elect King Arthur, so they don't obey!");
         EvaluationForHouse e2 = e1;
         Assert.assertEquals(e1.hashCode(),e2.hashCode());
     }
 
     @Test
     public void testHashCodeWhenNotEquals() {
-        EvaluationForHouse e1 = new EvaluationForHouse(0,"The old man killed Sir Kalahan.");
-        EvaluationForHouse e2 = new EvaluationForHouse(0,"The black knight had just a small skratch, he said.");
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        EvaluationForHouse e1 = new EvaluationForHouse(l1,0,"The old man killed Sir Kalahan.");
+        EvaluationForHouse e2 = new EvaluationForHouse(l2,0,"The black knight had just a small skratch, he said.");
         Assert.assertNotEquals(e1.hashCode(),e2.hashCode());
     }
 
     @Test
     public void testEqualsWhenNotEquals() {
-        EvaluationForHouse e1 = new EvaluationForHouse(2,"The black knight didn't let us pass.");
-        EvaluationForHouse e2 = new EvaluationForHouse(0,"The knights who say \"ni\", demanded a srawberry.");
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        EvaluationForHouse e1 = new EvaluationForHouse(l1,2,"The black knight didn't let us pass.");
+        EvaluationForHouse e2 = new EvaluationForHouse(l2,0,"The knights who say \"ni\", demanded a srawberry.");
         Assert.assertFalse(e1.equals(e2));
     }
 
     @Test
     public void testGetAverageFeedbackForHouse() {
-        new EvaluationForHouse(4,"");
-        new EvaluationForHouse(0,"");
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        new EvaluationForHouse(l1,4,"");
+        new EvaluationForHouse(l2,0,"");
         Assert.assertEquals(2,EvaluationForHouse.getAverageFeedbackForHouse(),0.000001);
     }
 
     @Test
     public void testGetNumOfEvaluationsForHouse() {
-        new EvaluationForHouse(4);
-        new EvaluationForHouse(4,"");
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        new EvaluationForHouse(l1,4);
+        new EvaluationForHouse(l2,4,"");
         Assert.assertEquals(2,EvaluationForHouse.getNumOfEvaluations());
     }
 
--- AirBmB/app/src/test/java/com/example/airbmb/Model/EvaluationForRenterTest.java
+++ AirBmB/app/src/test/java/com/example/airbmb/Model/EvaluationForRenterTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 public class EvaluationForRenterTest {
 
     @Before
@@ -13,22 +15,40 @@ public class EvaluationForRenterTest {
 
     @Test
     public void testRemoveWithTwoEval() {
-        EvaluationForRenter e1 = new EvaluationForRenter(5);
-        new EvaluationForRenter(1,"King Arthur wanted to take our Holy Grail.",100);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e1 = new EvaluationForRenter(l,5);
+        new EvaluationForRenter(l,1,"King Arthur wanted to take our Holy Grail.",100);
         e1.remove();
         Assert.assertEquals(1,EvaluationForRenter.getNumOfEvaluations());
     }
 
     @Test
     public void testRemoveWithOneEval() {
-        EvaluationForRenter e = new EvaluationForRenter(5);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e = new EvaluationForRenter(l,5);
         e.remove();
         Assert.assertEquals(0,EvaluationForRenter.getNumOfEvaluations());
     }
 
     @Test
     public void testRemoveWithNoEval() {
-        EvaluationForRenter e = new EvaluationForRenter(5);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e = new EvaluationForRenter(l,5);
         e.remove();
         e.remove();
         Assert.assertEquals(0,EvaluationForRenter.getNumOfEvaluations());
@@ -36,26 +56,50 @@ public class EvaluationForRenterTest {
 
     @Test
     public void testEvaluationForRenterInt() {
-        new EvaluationForRenter(5);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        new EvaluationForRenter(l,5);
         Assert.assertEquals(1,EvaluationForRenter.getNumOfEvaluations());
     }
 
     @Test
     public void testEvaluationForRenterIntStringInt() {
-        new EvaluationForRenter(0,"Police was called in front of the Castle of Aaarg, because of the knights.",2000);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        new EvaluationForRenter(l,0,"Police was called in front of the Castle of Aaarg, because of the knights.",2000);
         Assert.assertEquals(1,EvaluationForRenter.getNumOfEvaluations());
     }
 
     @Test
     public void testSetAndGetRepairsValue() {
-        EvaluationForRenter e = new EvaluationForRenter(2,"The Vikings sung all the time about Spam.",20);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e = new EvaluationForRenter(l,2,"The Vikings sung all the time about Spam.",20);
         e.setRepairsValue(200); //They keep singing...
         Assert.assertEquals(200,e.getRepairsValue());
     }
 
     @Test
     public void testSetAndGetCommentsAndFeedBack() {
-        EvaluationForRenter e = new EvaluationForRenter(4,"The rabbit in the cave has two sharp teeths!",20);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e = new EvaluationForRenter(l,4,"The rabbit in the cave has two sharp teeths!",20);
         e.setComments("The rabbit killed five knights, Bors, Gawain and Hector!");
         e.setFeedback(1); //Don't worry, we have the Holy Hand Grenade!
         Assert.assertEquals(1,e.getFeedback());
@@ -64,48 +108,106 @@ public class EvaluationForRenterTest {
 
     @Test
     public void testHashCodeWhenEquals() {
-        EvaluationForRenter e1 = new EvaluationForRenter(2,"King Arthur oppressed the free peasants.",20);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e1 = new EvaluationForRenter(l,2,"King Arthur oppressed the free peasants.",20);
         EvaluationForRenter e2 = e1;
         Assert.assertEquals(e1.hashCode(),e2.hashCode());
     }
 
     @Test
     public void testHashCodeWhenNotEquals() {
-        EvaluationForRenter e1 = new EvaluationForRenter(2,"",20);
-        EvaluationForRenter e2 = new EvaluationForRenter(0,"",4);
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        EvaluationForRenter e1 = new EvaluationForRenter(l1,2,"",20);
+        EvaluationForRenter e2 = new EvaluationForRenter(l2,0,"",4);
         Assert.assertNotEquals(e1.hashCode(),e2.hashCode());
     }
 
     @Test
     public void testEqualsWhenNotEquals() {
-        EvaluationForRenter e1 = new EvaluationForRenter(2,"King Arthur oppressed the free peasants.",20);
-        EvaluationForRenter e2 = new EvaluationForRenter(0,"",4);
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        EvaluationForRenter e1 = new EvaluationForRenter(l1,2,"King Arthur oppressed the free peasants.",20);
+        EvaluationForRenter e2 = new EvaluationForRenter(l2,0,"",4);
         Assert.assertFalse(e1.equals(e2));
     }
 
     @Test
     public void testGetAverageFeedbackForRenter() {
-        new EvaluationForRenter(4,"The Hungarian visitor is arrogant.",500);
-        new EvaluationForRenter(0,"The Hungarian visitor hit me.",1000);
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        new EvaluationForRenter(l1,4,"The Hungarian visitor is arrogant.",500);
+        new EvaluationForRenter(l2,0,"The Hungarian visitor hit me.",1000);
         Assert.assertEquals(2,EvaluationForRenter.getAverageFeedback(),0.000001);
     }
 
     @Test
     public void testGetNumOfEvaluationsForRenter() {
-        new EvaluationForRenter(4);
-        new EvaluationForRenter(4,"The Magi visited the wrong stable.",1000);
+        LocalDate start1 = LocalDate.of(2019,4,25);
+        LocalDate start2 = LocalDate.of(2019,5,10);
+        LocalDate end1 = LocalDate.of(2019,5,2);
+        LocalDate end2 = LocalDate.of(2019,5,15);
+        Renter r1 = new Renter ("John" ,"Snow","got@info.gr", "01234567890123456","Revive", "Johny123");
+        Renter r2 = new Renter ("Khaleesi" ,"Targaryen","dragons@info.gr", "01234567890123456","QueenOfDragons", "iLoveDragons123");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l1 = new Lease(start1,end1,1500, r1, h);
+        Lease l2 = new Lease (start2,end2,1000,r2,h);
+        new EvaluationForRenter(l1,4);
+        new EvaluationForRenter(l2,4,"The Magi visited the wrong stable.",1000);
         Assert.assertEquals(2,EvaluationForRenter.getNumOfEvaluations());
     }
 
     @Test
     public void testDamagesWhenTrue() {
-        EvaluationForRenter e = new EvaluationForRenter(2,"I didn't like the Spam!",10);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e = new EvaluationForRenter(l,2,"I didn't like the Spam!",10);
         Assert.assertTrue(e.damages());
     }
 
     @Test
     public void testDamagesWhenFalse() {
-        EvaluationForRenter e = new EvaluationForRenter(1);
+        LocalDate start = LocalDate.of(2019,5,25);
+        LocalDate end = LocalDate.of(2019,5,30);
+        Renter r = new Renter ("name" ,"lastName","email@info.gr", "01234567890123456","username", "password");
+        Owner o = new Owner("name", "lastname", "owner@info.gr", "987654321032568","owner", "passOwner");
+        House h = new House ("city", "street", 50,100,3,2,1500,o);
+        Lease l = new Lease(start,end,1500, r, h);
+        EvaluationForRenter e = new EvaluationForRenter(l,1);
         Assert.assertFalse(e.damages());
     }
 
