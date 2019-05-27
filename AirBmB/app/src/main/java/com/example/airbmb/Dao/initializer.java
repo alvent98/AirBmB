62fcf6127b56fb151e06e95c847eae1c83dddece
DikeaSot <48369123+DikeaSot@users.noreply.github.com>
2019-05-27 18:19:17 +0300

    Update Initializer

--- AirBmB/app/src/main/java/com/example/airbmb/Dao/initializer.java
+++ AirBmB/app/src/main/java/com/example/airbmb/Dao/initializer.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 
 public abstract class Initializer  {
     protected abstract void eraseData();
-   
+
     @RequiresApi(api = Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
     public void prepareData() {
         eraseData();
@@ -26,38 +26,38 @@ public abstract class Initializer  {
         EvaluationForHouseDAO ehd = getEvaluationForHouseDAO();
         EvaluationForRenterDAO erd = getEvaluationForRenterDAO();
 
-		Owner owner1 = new Owner ("Christopher","Robin","christ@email.com","12345678","chr","123123");
-		Owner owner2 = new Owner ("Nick","Pos","nickpos@email.com","87654321","nick12","123443");
-		Owner owner3 = new Owner ("Manos","Tounis","ManosT@email.com","78654321","Manos1996","444663");
-		
-		House house1 = new House ("kabala", "kabalas",32,4,2,3,250,owner1);
-		House house2 = new House ("evoia","xalkidas",17,72,3,3,300,owner2);
-		House house3 = new House ("patra","kozanis",9,32,1,2,170,owner3);
-		House house4 = new House ("tripoli","lakonias",57,7,0,4,320,owner2);
-		
+        Owner owner1 = new Owner ("Christopher","Robin","christ@email.com","12345678","chr","123123");
+        Owner owner2 = new Owner ("Nick","Pos","nickpos@email.com","87654321","nick12","123443");
+        Owner owner3 = new Owner ("Manos","Tounis","ManosT@email.com","78654321","Manos1996","444663");
+
+        House house1 = new House ("kabala", "kabalas",32,4,2,3,250,owner1);
+        House house2 = new House ("evoia","xalkidas",17,72,3,3,300,owner2);
+        House house3 = new House ("patra","kozanis",9,32,1,2,170,owner3);
+        House house4 = new House ("tripoli","lakonias",57,7,0,4,320,owner2);
+
         Renter renter1= new Renter ("tom","possibol","tompos@email.com","12222237","possibol15","tom123");
-		Renter renter2= new Renter ("kostantinos","pixinis","kopi@email.com","1598764","kostpi","56009");
-		Renter renter3= new Renter ("takis","mpar","takmpar@email.com","22209767","takaros","957609");
-		Renter renter4= new Renter ("alex","kouper","kouper@email.com","8757599","alex","102938");
+        Renter renter2= new Renter ("kostantinos","pixinis","kopi@email.com","1598764","kostpi","56009");
+        Renter renter3= new Renter ("takis","mpar","takmpar@email.com","22209767","takaros","957609");
+        Renter renter4= new Renter ("alex","kouper","kouper@email.com","8757599","alex","102938");
 
         LocalDate start = LocalDate.of(2019, 4, 20);
         LocalDate end = LocalDate.of(2019, 4, 26);
-		Lease lease1= new Lease(start,end,0,renter1,house1);
-		Lease lease2= new Lease(start,end,0,renter2,house2);
-		Lease lease3= new Lease(start,end,0,renter3,house3);
-		Lease lease4= new Lease(start,end,0,renter4,house4);
+        Lease lease1= new Lease(start,end,0,renter1,house1);
+        Lease lease2= new Lease(start,end,0,renter2,house2);
+        Lease lease3= new Lease(start,end,0,renter3,house3);
+        Lease lease4= new Lease(start,end,0,renter4,house4);
 
-		EvaluationForHouse eh1 = new EvaluationForHouse(lease1,1,"dont go there  its the most hidious owner ever");
-		EvaluationForHouse eh2 = new EvaluationForHouse(lease2,5,"perfect everything good");
-		EvaluationForHouse eh3 = new EvaluationForHouse(lease3,4,"nice house but no wifi");
-		EvaluationForHouse eh4 = new EvaluationForHouse(lease4,5,"everything was fine");
+        EvaluationForHouse eh1 = new EvaluationForHouse(lease1,1,"dont go there  its the most hidious owner ever");
+        EvaluationForHouse eh2 = new EvaluationForHouse(lease2,5,"perfect everything good");
+        EvaluationForHouse eh3 = new EvaluationForHouse(lease3,4,"nice house but no wifi");
+        EvaluationForHouse eh4 = new EvaluationForHouse(lease4,5,"everything was fine");
 
-		EvaluationForRenter er1 = new EvaluationForRenter(lease1,1,"dont rent to him its the most hidious renter ever",560);
+        EvaluationForRenter er1 = new EvaluationForRenter(lease1,1,"dont rent to him its the most hidious renter ever",560);
         EvaluationForRenter er2 = new EvaluationForRenter(lease2,4,"enough cooperative renter",0);
-		EvaluationForRenter er3 = new EvaluationForRenter(lease3,4,"very nice but they left trash behind",0);
-		EvaluationForRenter er4 = new EvaluationForRenter(lease3,5,"the kindest renter i've ever had!!",0);
+        EvaluationForRenter er3 = new EvaluationForRenter(lease3,4,"very nice but they left trash behind",0);
+        EvaluationForRenter er4 = new EvaluationForRenter(lease3,5,"the kindest renter i've ever had!!",0);
 
-		od.save(owner1);
+        od.save(owner1);
         od.save(owner2);
         od.save(owner3);
 
@@ -89,7 +89,7 @@ public abstract class Initializer  {
     public abstract OwnerDAO getOwnerDAO();
 
     public abstract HouseDAO getHouseDAO();
-    
+
     public abstract RenterDAO getRenterDAO();
 
     public abstract LeaseDAO getLeaseDAO();
