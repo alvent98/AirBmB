dc92fb889d4c779ab56152843d31813d86cc9c8b
xideasxristos <48369600+xideasxristos@users.noreply.github.com>
2019-05-27 17:44:52 +0300

    Initializer

--- AirBmB/app/src/main/java/com/example/airbmb/Dao/initializer.java
+++ AirBmB/app/src/main/java/com/example/airbmb/Dao/initializer.java
@@ -1,51 +1,14 @@
-package com.mgiandia.library.dao;
+package project_airbmb.airbmb.DAO;
 
-import com.mgiandia.library.contacts.EmailAddress;
-import com.mgiandia.library.domain.*;
-import com.mgiandia.library.util.Money;
+package com.example.airbmb.DAO;
+
+//import com.mgiandia.library.contacts.EmailAddress;
+//import com.mgiandia.library.domain.*;
+//import com.mgiandia.library.util.Money;
 
 
-/**
- * Κλάση που αναλαμβάνει να αρχικοποιήσει τα δεδομένα της βάσης δεδομένων<p>
- * Βοηθητική κλάση που παρέχει δεδομένα για τα παραδείγματα και τις δοκιμασίες ελέγχου<p>
- * Βιβλία
- * <p>
- * Booch, G.,  Rumbaugh, J., Jacobson I., The Unified Modeling Language User Guide , 2nd ed., Addison Wesley, 2005 
- * <p>
- * Fowler, M., UML Distilled, 3rd ed., Addison-Wesley, 2004
- * <p>
- * Fowler, M., Refactoring: Improving the Design of Existing Code. Addison-Wesley, 1999
- * <p>
- * Αντίτυπα 
- * <p>
- * 1 - The Unified Modeling Language User Guide
- * <p>
- * 2 -  UML Distilled
- * <p>
- * 3 - Refactoring: Improving the Design of Existing Code
- * <p>
- * 4 - The Unified Modeling Language User Guide
- * <p>
- * 5 - UML Distilled
- * <p>
- * Δανειζόμενοι
- * <p>
- * 1, Μανόλης Γιακουμάκης, Καθηγητής
- * <p>
- * 2, Νίκος Διαμαντίδης, Φοιτητής
- * <p>
- *@author Νίκος Διαμαντίδης
- *    
- */
 public abstract class Initializer  {
-    public static final int GIAKOUMAKIS_ID = 1;
-    public static final int DIAMANTIDIS_ID = 2;
-    
-    public static int UML_USER_GUIDE_ID1 = 1;
-    public static int UML_DISTILLED_ID1 = 2;
-    public static int UML_REFACTORING_ID = 3;
-    public static int UML_USER_GUIDE_ID2 = 4;
-    public static int UML_DISTILLED_ID2 = 5;
+
     
     //διαγράφουμε όλα τα δεδομένα στη βάση δεδομένων
     protected abstract void eraseData();
@@ -83,7 +46,6 @@ public abstract class Initializer  {
 		 EvaluationForHouse EH3 = new EvaluationForHouse(lease3,4,"nice house but no wifi"); 
 		 EvaluationForHouse EH4 = new EvaluationForHouse(lease4,5,"everything was fine");
 		
-		public EvaluationForRenter(Lease lease, int feedback, String comments, int repairsValue)
         EvaluationForRenter ER1 = new EvaluationForRenter(lease1,1,"dont rent to him its the most hidious renter ever",560);
         EvaluationForRenter ER2 = new EvaluationForRenter(lease2,4,"enough cooperative renter",0);
 		EvaluationForRenter ER3 = new EvaluationForRenter(lease3,4,"very nice but they left trash behind",0);
